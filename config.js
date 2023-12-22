f9c59a311ea57da5938cb88a739b2fd5be16a641
Shubham Roy <101193470+ROY1419@users.noreply.github.com>
2023-12-22 17:47:46 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const api_config = {
+    port : 5001,
+    dbUrl : "mongodb+srv://roy1914:maa12345@roy.yifrrgb.mongodb.net/?retryWrites=true&w=majority"
+}
+
+module.exports = api_config;
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const userRouter = require("./routes/userRouter");
+const cors = require("cors");
+const api_config = require("./config");
+
+const app = express();
+const port = api_config.port;
+
+// for parsing request data
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
+
+//address
+// app.use("/meme", memeRouter);
+app.use("/user", userRouter);
+
+app.get("/home", (req, res) => {
+  console.log("request at home");
+  res.send("the wait is over!");
+});
+
+app.listen(port, () => {
+  console.log("server started");
+});
